5ad7223cb8bfc27576ff28444addb0b1bc3c48ba
Navdeep Natt <nattnavdeep41@gmail.com>
2024-12-18 15:41:01 -0600

    Something's Wrong

--- data_parsing.py
+++ data_parsing.py
@@ -1,19 +1,33 @@
 """
 Parsing Trip Data Input from Terminal (for now)
 """
-from enum import Enum
+from typing import 
 
-
-def read_trucker_info() -> dict[str, int, int]:
+def read_trucker_info():
     """
     Reads trucker info
     """
     name = input("Enter your name: ")
     id = input("Enter your trucker ID: ")
     home_base = input("Enter your home base: ")
-    return {"name": name, "id": id, "home base": home_base}
+    return (name, id, home_base)
 
-
+def read_trip_info():
+    """
+    Reads trip info
+    """
+    truck_id = input("Enter ID of Truck for Trip: ")
+    day = input("Enter day of trip (mm/dd/yyyy): " )
+    start_loc = input("Enter start location: ")
+    end_loc = input("Enter end location: ")
+    print("Begin entering stops along way; when finished, enter 'x'")
+    stops_along_way = []
+    user_input = input("Enter Stop: ")
+    while user_input != 'x':
+        stops_along_way.append(user_input)
+    return (truck_id, day, start_loc, end_loc, stops_along_way)
+    
 if __name__ == "__main__":
     read_trucker_info()
     print("I'm over here")
+    
--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import os
 
-def foo():
-    pass
-
 def main():
     return 0
